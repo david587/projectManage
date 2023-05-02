7b327496d3e37aaa6f60954d4704daf679b6039f
David <davidbarath08@gmail.com>
2023-05-02 18:10:03 +0200

    firebase added and authcontext added, but no used.

---
+++ src/firebase/config.jsx
@@ -0,0 +1,28 @@
+//firebase cli->firbaseinit(rules,hosting & storage)
+
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/auth"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyClIRlExBDU7GG0EQz4NFYS8fEs6kr5avc",
+    authDomain: "project-manage-2fc1c.firebaseapp.com",
+    projectId: "project-manage-2fc1c",
+    storageBucket: "project-manage-2fc1c.appspot.com",
+    messagingSenderId: "818542765072",
+    appId: "1:818542765072:web:7fb3d303d04485d2d55695",
+    measurementId: "G-QQLZX2XRQR"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  //init services
+  const projectFirestore = firebase.firestore()
+  const projectAuth = firebase.auth()
+
+  //timestamp
+  const timestamp = firebase.firestore.Timestamp
+
+  export { projectFirestore, projectAuth , timestamp}
