f817e25f718d0cca9661dadd2d948dfdd2a63019
David <davidbarath08@gmail.com>
2023-05-16 20:24:08 +0200

    project delete is added

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,24 +1,40 @@
-import React from 'react'
-import Avatar from '../../components/Avatar'
+import Avatar from "../../components/Avatar"
+import { useFirestore } from "../../hooks/useFirestore"
+import { useHistory } from 'react-router-dom'
+import { useAuthContext } from "../../hooks/useAuthContext"
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore('projects')
+  const { user } = useAuthContext()
+  const history = useHistory()
+
+  const handleClick = () => {
+    deleteDocument(project.id)
+    history.push('/')
+  }
+
   return (
     <div>
-        <div className="project-summary">
-            <h2 className="page-title">{project.name}</h2>
-            <p className='due-date'>
-                Project due by {project.dueDate.toDate().toDateString()}
-            </p>
-            <p className='details'>
-                {project.details}
-            </p>
-            <h4>Project is assigned to:</h4>
-            {project.assignedUsersList.map(user => (
-                <div key={user.id}>
-                    <Avatar src={user.photoURL} ></Avatar>
-                </div>
-            ))}
+      <div className="project-summary">
+        <h2 className="page-title">{project.name}</h2>
+        <p className="due-date">
+          Project due by {project.dueDate.toDate().toDateString()}
+        </p>
+        <p className="details">
+          {project.details}
+        </p>
+        <h4>Project assigned to:</h4>
+        <div className="assigned-users">
+          {project.assignedUsersList.map(user => (
+            <div key={user.id}>
+              <Avatar src={user.photoURL} />
+            </div>
+          ))}
         </div>
+      </div>
+      {user.uid === project.createdBy.id && (
+        <button className="btn" onClick={handleClick}>Mark as Complete</button>
+      )}
     </div>
   )
 }
